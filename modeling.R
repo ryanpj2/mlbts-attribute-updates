240594c494d1732766a91ae87e26ed66dfeb20cd
Ryan <ryanpj2@illinois.edu>
2023-10-22 17:00:30 -0500

    loosened PA reqs for data

--- modeling.R
+++ modeling.R
@@ -1,9 +1,9 @@
 load("live-batters-full.rda")
 
-live_batters_full = live_batters_full %>% 
-  rename(BBperc = 'BB%', Kperc = 'K%')
+live_batters_trn = live_batters_full %>% 
+  rename(BBperc = 'BB%', Kperc = 'K%') %>% 
+  filter(PA > 50 & PAvL > 25)
 
-lm.fit = lm(ovr ~ fielding_ability + arm_strength + arm_accuracy + reaction_time +
-              speed + baserunning_ability + BBperc + Kperc + AVGvR + ISOvR + AVGvL +
-              ISOvL + AVGwRISP, data = live_batters_full)
+lm.fit = lm(ovr ~ fielding_ability + BBperc + Kperc + AVGvR + ISOvR + AVGvL +
+              ISOvL, data = live_batters_trn)
 summary(lm.fit)
