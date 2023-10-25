f645b68c78f46b7564cdd968d7d01028938b65f4
Ryan <ryanpj2@illinois.edu>
2023-10-25 16:05:40 -0500

    adjusted model and PA filters

--- modeling.R
+++ modeling.R
@@ -1,9 +1,19 @@
 load("live-batters-full.rda")
+library(tidyverse)
 
 live_batters_trn = live_batters_full %>% 
   rename(BBperc = 'BB%', Kperc = 'K%') %>% 
   filter(PA > 50 & PAvL > 25)
 
+live_batters = live_batters_full %>% 
+  rename(BBperc = 'BB%', Kperc = 'K%') %>% 
+  select(ovr, fielding_ability:reaction_time, speed, baserunning_ability, BBperc:AVGwRISP)
+
 lm.fit = lm(ovr ~ fielding_ability + BBperc + Kperc + AVGvR + ISOvR + AVGvL +
-              ISOvL, data = live_batters_trn)
+              ISOvL, data = live_batters_full)
 summary(lm.fit)
+
+lm.fit2 = lm(ovr ~ ., data = live_batters)
+summary(lm.fit2)
+
+predict(lm.fit2, newdata = live_batters[548,])
